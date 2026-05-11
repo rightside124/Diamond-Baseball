5fbf28a36edc7030317198825a58f222653993b8
Matthew! <149981660+rightside124@users.noreply.github.com>
2026-05-11 22:29:32 +0800

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,14 +1,14 @@
 function updateUI(){
 
   document.getElementById("teams").innerText =
-    `${STATE.player.team} ${STATE.game.score[0]} | ${STATE.game.score[1]} OPP`;
+    `CHI ${STATE.game.score[0]} | ${STATE.game.score[1]} OPP`;
 
   document.getElementById("count").innerText =
     `${STATE.game.balls}-${STATE.game.strikes}`;
 
   document.getElementById("bases").innerText =
-    STATE.game.bases.map(b => b?"◆":"◇").join(" ");
+    STATE.game.bases.map(b => b ? "◆":"◇").join(" ");
 
   document.getElementById("pitch-info").innerText =
-    `${STATE.game.pitch.type} ${STATE.game.pitch.speed}`;
+    `${STATE.game.pitch?.type || "--"} ${STATE.game.pitch?.speed || ""}`;
 }
